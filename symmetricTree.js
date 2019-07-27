fe6a873b9a630bc96ca2a79a37f8dab332adc0d3
github username <kahyap13@gmail.com>
2019-07-26 23:12:28 -0400

    Runtime: 56 ms, faster than 90.79% of JavaScript online submissions for Symmetric Tree. Memory Usage: 35.5 MB, less than 71.70% of JavaScript online submissions for Symmetric Tree.

---
+++ symmetricTree.js
@@ -0,0 +1,47 @@
+// Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+//
+// For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+//
+//     1
+//    / \
+//   2   2
+//  / \ / \
+// 3  4 4  3
+//
+//
+// But the following [1,2,2,null,3,null,3] is not:
+//
+//     1
+//    / \
+//   2   2
+//    \   \
+//    3    3
+//
+//
+// Note:
+// Bonus points if you could solve it both recursively and iteratively.
+//
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if(!root) return true
+
+    let dfs = (leftNode, rightNode) => {
+        if(!leftNode && !rightNode) return true
+        if((!leftNode && rightNode) || (leftNode && !rightNode)) return false
+        if(leftNode.val !== rightNode.val) return false
+        return dfs(leftNode.left, rightNode.right) && dfs(leftNode.right, rightNode.left)
+    }
+
+    return dfs(root.left, root.right)
+};
