0ce1be50897528ef773d4d94c43c1c79d67334c4
Kah919 <kahyap13@gmail.com>
2020-08-24 02:19:51 -0400

    Create a function to check if the two numbers are single digit or the same

--- CodeSignal/compareReversePairs.js
+++ CodeSignal/compareReversePairs.js
@@ -8,12 +8,12 @@ const arr = [1, 20, 2, 11] // result should be 7
 // Given an array of non-negative integers arr, your task is to count the number of pairs (i, j) such that i <= j and
 // arr[i] + rev(arr[j]) = arr[j] + rev(arr[i])
 
-const compareReversePairs = () => {
+const oppositeSums = () => {
     let result = 0
 
     for(let i = 0; i < arr.length; i++) {
         for(let j = i; j < arr.length; j++) {
-            if((arr[i] === arr[j]) || (arr[i] < 10 arr[j] < 10)) {
+            if(isSingleOrSame(arr[i], arr[j])) {
                 result++
             }
             
@@ -21,4 +21,12 @@ const compareReversePairs = () => {
     }
 
     return result
+}
+
+const isSingleOrSame = (num1, num2) => {
+    if((num1 === num2) || (num1 < 10 && num2 < 10)) {
+        return true
+    } else {
+        return false
+    }
 }
