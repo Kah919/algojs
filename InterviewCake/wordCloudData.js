c9d271a1007a68e3059d9121e82401973631df23
Kah919 <kahyap13@gmail.com>
2020-09-03 18:40:05 -0400

    Debugged error where spaces were added to the map

--- InterviewCake/wordCloudData.js
+++ InterviewCake/wordCloudData.js
@@ -14,17 +14,16 @@ class WordCloudData {
         let curr = inputString[i]
         
         if(startIdx === 0 && endIdx === 0 && this.isLetter(curr)) {
-          console.log(`This is the start of a new word at ${curr}`)
           startIdx = i
           endIdx = i + 1
         } else if(i === inputString.length - 1 && this.isLetter(curr)) {
           const word = inputString.slice(startIdx)
-          console.log(word)
-        } else if(this.isLetter(curr)) {
+          this.normalize(word)
+        } else if(this.isLetter(curr) || curr === '\'') {
           endIdx++
         } else if(!this.isLetter(curr)) {
           const word = inputString.slice(startIdx, endIdx)
-          console.log(word)
+          this.normalize(word)
           startIdx = 0
           endIdx = 0
         } 
@@ -33,11 +32,39 @@ class WordCloudData {
     }
     
     isLetter(letter) {
-      const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
       return letters.indexOf(letter) >= 0
     }
+
+    addToMap(word) {
+      if(this.wordsToCounts.has(word)) {
+        this.wordsToCounts.set(word, this.wordsToCounts.get(word) + 1)
+      } else {
+        this.wordsToCounts.set(word, 1)
+      }
+    }
+
+    normalize(word) {
+      const capitalize = word.charAt(0).toUpperCase() + word.slice(1)
+      const lowercase = word.charAt(0).toLowerCase() + word.slice(1)
+      if(word === lowercase && this.wordsToCounts.has(capitalize)) {
+        this.wordsToCounts.set(lowercase, this.wordsToCounts.get(capitalize) + 1)
+        this.wordsToCounts.delete(capitalize)
+      } else if(word === capitalize && this.wordsToCounts.has(lowercase)) {
+        this.wordsToCounts.set(lowercase, this.wordsToCounts.get(lowercase) + 1)
+      } else if(word !== ''){
+        this.addToMap(word)
+      }
+    }
+    
   
   }
 
-  const test = new WordCloudData('Just a test')
-  console.log(test.populateWordsToCounts)
+  // const test = new WordCloudData('Strawberry short cake? Yum!')
+  // const test2 = new WordCloudData('I like cake')
+  // const test3 = new WordCloudData('Chocolate cake for dinner and pound cake for dessert  ')
+  // const test4 = new WordCloudData('Dessert - mille-feuille cake')
+  // const test5 = new WordCloudData('Mmm...mmm...decisions...decisions')
+  const test6 = new WordCloudData("Allie's Bakery: Sasha's Cakes")
+  console.log(test6.populateWordsToCounts)
+  console.log(test6.wordsToCounts)
