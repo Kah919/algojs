a974c7210275e24687032f4de53dae3f3e369441
Kah919 <kahyap13@gmail.com>
2020-09-04 16:14:04 -0400

    highestProductOfThree - Greedy Approach

---
+++ InterviewCake/highestProductOfThree.js
@@ -0,0 +1,40 @@
+// Given an array of integers, find the highest product you can get from three of the integers.
+// The input arrayOfInts will always have at least three integers.
+
+function highestProductOf3(arrayOfInts) {
+    if(arrayOfInts.length < 3) throw error()
+    let lowest = Math.min(arrayOfInts[0], arrayOfInts[1])
+    let highest = Math.max(arrayOfInts[0], arrayOfInts[1])
+    
+    let lowestProductOfTwo = arrayOfInts[0] * arrayOfInts[1]
+    let highestProductOfTwo = arrayOfInts[0] * arrayOfInts[1]
+    
+    let highestProductOfThree = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2]
+    
+    for(let i = 2; i < arrayOfInts.length; i++) {
+      const curr = arrayOfInts[i]
+      
+      highestProductOfThree = Math.max(
+        highestProductOfTwo * curr,
+        lowestProductOfTwo * curr,
+        highestProductOfThree
+      )
+      
+      lowestProductOfTwo = Math.min(
+        lowestProductOfTwo,
+        lowest * curr,
+        highest * curr
+      )
+      
+      highestProductOfTwo = Math.max(
+        highestProductOfTwo,
+        lowest * curr,
+        highest * curr
+      )
+      
+      lowest = Math.min(lowest, curr)
+      highest = Math.max(highest, curr)
+    }
+    
+    return highestProductOfThree
+  }
